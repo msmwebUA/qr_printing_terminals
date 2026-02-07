786263b28266d45ee972b2879df2538e39746af3
Sergii Mishchaniuk <mishchaniuk@gmail.com>
2026-02-07 15:05:01 +0200

    Create class ScanCard

---
+++ terminal_app/scan_card.py
@@ -0,0 +1,27 @@
+from mfrc522 import SimpleMFRC522
+import RPi.GPIO as GPIO
+
+class ScanCard():
+
+  def __init__(self, config: object):
+    self.config = config
+
+  """Read data from rfid card. Return list with 2 elements: 
+      list[0] is result code (0 - error, 1 - success), 
+      list[1] is error message or text from card"""
+  def read(self) -> list:
+    counter = 0
+    try:
+      reader = SimpleMFRC522()
+      while True:
+        id, text = reader.read()
+        if (id and text):
+          return [1, text]
+        counter += 1
+        # limit false scans
+        if counter > self.config.max_scans:
+          raise Exception("Exceeded limit of false scans")
+    except Exception as e:
+      return [0, e]
+    finally:
+      GPIO.cleanup()
