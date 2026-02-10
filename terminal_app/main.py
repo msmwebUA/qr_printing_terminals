abba17538c7921f4b7c43bed2defbcb34af5827f
Sergii Mishchaniuk <mishchaniuk@gmail.com>
2026-02-10 16:48:00 +0200

    Add test code to main.py

---
+++ terminal_app/main.py
@@ -0,0 +1,12 @@
+from scan_card import ScanCard
+
+# application config
+from config import Config
+
+# init configuration
+config = Config()
+
+scan = ScanCard(config)
+# read returns list with 2 elements [code 0 or 1, error message or card data]
+scan.read()
+
