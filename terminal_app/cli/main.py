b60d79e32c6b45dd12dde0acc6e328a573c62e02
Sergii Mishchaniuk <mishchaniuk@gmail.com>
2026-02-22 21:09:06 +0200

    Update main.py for running ui app. Move CLI version into cli directory

---
+++ terminal_app/cli/main.py
@@ -0,0 +1,56 @@
+from scan_card import ScanCard
+from print_label import PrintLabel
+from label import Label
+from database import Database
+# application config
+from config import Config
+# init configuration 
+config = Config()
+
+def main() -> None:
+  # init database
+  db = Database(config)
+  # scan card
+  print("Scan your card...")
+  scan_obj = ScanCard(config)
+  # read method returns list with 2 elements [code 0 or 1, error message or card data]
+  scan_feedback = scan_obj.read()
+  if scan_feedback[0] == 1:
+    emp_id = scan_feedback[1]
+    print("Scan was successful")
+    # create label and print objects
+    label_obj = Label(config)
+    print_obj = PrintLabel(config)
+    # number of copies from user input
+    copies = print_obj.getCopies()
+    # create and print labels
+    print_feedback = print_obj.print(label_obj, emp_id, copies)
+    if print_feedback[0] == 1:
+      # add all printed label_ids to database
+      printed_data = print_feedback[1]
+      rows = []
+      for label_id in printed_data["label_ids"]:
+        row = int(f"{printed_data['emp_id']}{label_id}")
+        rows.append((row,))
+      db.addLabelId(rows)
+      db_msg = None
+      print("Label was sent to printer")
+    else:
+      db_msg = str(print_feedback[1])
+      # print printer error message
+      print(print_feedback[1])
+      # inform user about common errors in clear way
+      if 'Errno 110' or 'Failed to print' in db_msg:
+        print("Something wrong with printer. Are there labels in roll or is cover closed?")
+      if 'Device not found' in db_msg:
+        print("Check connection to printer")
+    # add log entry to db
+    db.addLogEntry(emp_id, str(copies), db_msg)
+  else:
+    # add log entry to db
+    db.addLogEntry(0, 0, str(scan_data[1]))
+    # print scan error message
+    print(scan_data[1])
+
+if __name__ == "__main__":
+  main()
