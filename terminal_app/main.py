bd5af576272090a0473339633a6264ee9708ca35
Sergii Mishchaniuk <mishchaniuk@gmail.com>
2026-02-26 12:31:00 +0200

    Make window active and move on top after program start

--- terminal_app/main.py
+++ terminal_app/main.py
@@ -6,6 +6,8 @@ def main():
   app = QApplication(sys.argv)
   window = App()
   window.show()
+  window.raise_() # window on top
+  window.activateWindow() # activate window to get focus
   sys.exit(app.exec())
 
 if __name__ == "__main__":
