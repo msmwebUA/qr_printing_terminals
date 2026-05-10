3f9a52e357c572c98d19ab5dc804ef691ca7ab3d
Sergii Mishchaniuk <mishchaniuk@gmail.com>
2026-05-10 12:13:03 +0300

    Modify latest_update_script: autodetect OS and use requirements.txt based on it

--- terminal_app/latest_update_script.sh
+++ terminal_app/latest_update_script.sh
@@ -22,8 +22,23 @@ echo "Step 3: Installing/Updating libraries from requirements.txt..."
 # Check if venv exists before activating
 if [ -f "$VENV_PATH" ]; then
     source "$VENV_PATH"
+    # Detect Debian/Raspberry Pi OS codename
+    OS_CODENAME=$(grep VERSION_CODENAME /etc/os-release | cut -d= -f2)
+    # Select requirements file
+    case "$OS_CODENAME" in
+        trixie)
+            REQUIREMENTS_FILE="$REPO_DIR/terminal_app/requirements/trixie/requirements.txt"
+            ;;
+        bookworm)
+            REQUIREMENTS_FILE="$REPO_DIR/terminal_app/requirements/bookworm/requirements.txt"
+            ;;
+        *)
+            REQUIREMENTS_FILE="$REPO_DIR/terminal_app/requirements/requirements.txt"
+            echo "Cannot find requirements.txt for $OS_CODENAME. I will use default path."
+            ;;
+    esac
     pip install --upgrade pip
-    pip install -r "$REPO_DIR/terminal_app/requirements/requirements.txt"
+    pip install -r "$REQUIREMENTS_FILE"
 else
     echo "Warning: Virtual environment not found at $VENV_PATH. Skipping library installation."
 fi
